63441c17de1d6497b1e8030166d1d84148c31111
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-12-06 07:04:20 +0000

    mongodb page finished

--- controllers/tutorial_controller.js
+++ controllers/tutorial_controller.js
@@ -3,3 +3,7 @@
 exports.home = function(req,res){
   res.render('pages/tutorial', { title: 'Express' });
 };
+
+exports.mongodb_get = function(req,res){
+  res.render('pages/mongodb');
+}
--- routes/tutorial.js
+++ routes/tutorial.js
@@ -5,4 +5,7 @@ var tutorialController = require('../controllers/tutorial_controller');
 /* GET tutorial page. */
 tutorialRoute.get('/', tutorialController.home);
 
+/* Get tutorial/node */
+tutorialRoute.get('/mongodb',tutorialController.mongodb_get);
+
 module.exports = tutorialRoute;
