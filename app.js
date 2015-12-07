a4e3aa2675d7ad1a1fb06e4790e9ef669aac9b84
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-12-07 21:56:26 +0000

    funciona de nuevo en heroku

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var tutorialRoute = require('./routes/tutorial');
-var usuariosRoute = require('./routes/usuarios');
-var registrarRoute = require('./routes/registrar');
-
-var mongoose = require('mongoose');
-
 
 var expressLayouts = require('express-ejs-layouts');
 var app = express();
@@ -32,8 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/tutorial', tutorialRoute);
-app.use('/usuarios', usuariosRoute);
-app.use('/registrar', registrarRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/registrar.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var registrarRoute = express.Router();
-var registrarController = require('../controllers/registrar_controller');
-
-/* GET tutorial page. */
-registrarRoute.get('/', registrarController.home);
-registrarRoute.post('/', registrarController.registrar);
-
-module.exports = registrarRoute;
