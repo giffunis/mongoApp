76c8a0f31cfc1607820136648de916a764bf5782
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-12-10 14:46:13 +0000

    some changes in registrar controller

--- controllers/registrar_controller.js
+++ controllers/registrar_controller.js
@@ -15,10 +15,11 @@ exports.registrar = function(req,res){
     if(err){
       //entrada duplicada
       if(err.code == 11000)
-        return res.json({success: false, message: "Existe un usuario con el mismo correo"});
+        res.redirect('/');
+        // return res.json({success: false, message: "Existe un usuario con el mismo correo"});
       else
-        return res.send(err);
-    }
-    res.redirect('/usuarios');
+        return res.send(err.message);
+    }else{
+    res.redirect('/usuarios');}
   });
 };
