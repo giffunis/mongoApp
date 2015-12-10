f10e91c52eb1e1d5772bec07985c639e3b2adf44
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-12-10 13:23:27 +0000

    eliminado routes /tutorial y controller

--- controllers/tutorial_controller.js
+++
@@ -1,9 +0,0 @@
-
-/* GET home page. */
-exports.home = function(req,res){
-  res.render('pages/tutorial', { title: 'Express' });
-};
-
-exports.mongodb_get = function(req,res){
-  res.render('pages/mongodb');
-};
